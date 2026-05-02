f464f911a4acf03950509e986ed40ac580805a38
Etienne Scherrer <contact@etienne-scherrer.ch>
2026-05-02 19:25:17 +0200

    perf: replace backtracking solver with MRV heuristic and bitmask constraints

--- sudoku/common-functions.php
+++ sudoku/common-functions.php
@@ -23,28 +23,69 @@ function isSafe(array $board, int $row, int $col, int $num): bool {
     return true;
 }
 
-function solveSudoku(array &$board, int $row = 0, int $col = 0): bool {
-    if ($row == 9) {  // Successfully filled the grid
-        return true;
-    }
+function solveSudoku(array &$board): bool {
+    $rows  = array_fill(0, 9, 0);
+    $cols  = array_fill(0, 9, 0);
+    $boxes = array_fill(0, 9, 0);
 
-    if ($col == 9) {  // Move to the next row
-        return solveSudoku($board, $row + 1, 0);
-    }
-
-    if ((int)$board[$row][$col]['value'] !== 0) {  // Skip filled cells
-        return solveSudoku($board, $row, $col + 1);
-    }
-
-    for ($num = 1; $num <= 9; $num++) {
-        if (isSafe($board, $row, $col, $num)) {
-            $board[$row][$col]['value'] = $num;
-            if (solveSudoku($board, $row, $col + 1)) {
-                return true;
+    for ($r = 0; $r < 9; $r++) {
+        for ($c = 0; $c < 9; $c++) {
+            $v = $board[$r][$c]['value'];
+            if ($v !== 0) {
+                $bit = 1 << $v;
+                $rows[$r]  |= $bit;
+                $cols[$c]  |= $bit;
+                $boxes[intdiv($r, 3) * 3 + intdiv($c, 3)] |= $bit;
             }
-            $board[$row][$col]['value'] = 0;  // Backtrack
         }
     }
 
-    return false;  // No solution found from this configuration
+    return _solve($board, $rows, $cols, $boxes);
+}
+
+function _solve(array &$board, array &$rows, array &$cols, array &$boxes): bool {
+    $minCount = 10;
+    $bestRow  = -1;
+    $bestCol  = -1;
+
+    for ($r = 0; $r < 9; $r++) {
+        for ($c = 0; $c < 9; $c++) {
+            if ($board[$r][$c]['value'] !== 0) continue;
+            $used  = $rows[$r] | $cols[$c] | $boxes[intdiv($r, 3) * 3 + intdiv($c, 3)];
+            $count = 0;
+            for ($n = 1; $n <= 9; $n++) {
+                if (!($used & (1 << $n))) $count++;
+            }
+            if ($count === 0) return false;
+            if ($count < $minCount) {
+                $minCount = $count;
+                $bestRow  = $r;
+                $bestCol  = $c;
+                if ($count === 1) break 2;
+            }
+        }
+    }
+
+    if ($bestRow === -1) return true;
+
+    $box  = intdiv($bestRow, 3) * 3 + intdiv($bestCol, 3);
+    $used = $rows[$bestRow] | $cols[$bestCol] | $boxes[$box];
+
+    for ($n = 1; $n <= 9; $n++) {
+        if ($used & (1 << $n)) continue;
+        $bit = 1 << $n;
+        $board[$bestRow][$bestCol]['value'] = $n;
+        $rows[$bestRow]  |= $bit;
+        $cols[$bestCol]  |= $bit;
+        $boxes[$box]     |= $bit;
+
+        if (_solve($board, $rows, $cols, $boxes)) return true;
+
+        $board[$bestRow][$bestCol]['value'] = 0;
+        $rows[$bestRow]  &= ~$bit;
+        $cols[$bestCol]  &= ~$bit;
+        $boxes[$box]     &= ~$bit;
+    }
+
+    return false;
 }
