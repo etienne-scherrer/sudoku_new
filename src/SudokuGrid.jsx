49932b5a1d395f3c52a4acadf58b14faced6aa7e
Etienne Scherrer <contact@etienne-scherrer.ch>
2026-05-05 20:56:20 +0200

    feat: wire ImportModal into SudokuGrid

--- src/SudokuGrid.jsx
+++ src/SudokuGrid.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useSudoku from './hooks/useSudoku';
 import SudokuBoard from './components/SudokuBoard';
 import Controls from './components/Controls';
 import Modal from './components/Modal';
+import ImportModal from './components/ImportModal';
 
 function SudokuGrid() {
   const {
@@ -12,13 +13,29 @@ function SudokuGrid() {
     isModalOpen,
     loading,
     error,
+    importing,
+    importError,
     fetchGrid,
     solveSudoku,
+    importGrid,
     clearGrid,
     handleInputChange,
     handleKeyDown,
     dismissModal,
   } = useSudoku();
+
+  const [pendingImportFile, setPendingImportFile] = useState(null);
+
+  const handleImport = (file) => setPendingImportFile(file);
+
+  const handleImportConfirm = async () => {
+    const success = await importGrid(pendingImportFile);
+    if (success) setPendingImportFile(null);
+  };
+
+  const handleImportCancel = () => {
+    setPendingImportFile(null);
+  };
 
   const modalContent = () => {
     if (loading) return <p>Loading...</p>;
@@ -29,6 +46,13 @@ function SudokuGrid() {
 
   return (
     <div>
+      <ImportModal
+        file={pendingImportFile}
+        onConfirm={handleImportConfirm}
+        onCancel={handleImportCancel}
+        loading={importing}
+        error={importError}
+      />
       <Modal isOpen={isModalOpen} onClose={dismissModal}>
         {modalContent()}
       </Modal>
@@ -43,6 +67,7 @@ function SudokuGrid() {
         onClear={clearGrid}
         onGenerate={fetchGrid}
         onSolve={solveSudoku}
+        onImport={handleImport}
         loading={loading}
       />
     </div>
